b7032ab70e6612359f23f320054081138e8a8c83
HSx3 <hs.ssafy@gmail.com>
2019-04-23 06:40:49 +0000

    02_music detail serializer

--- musics/serializers.py
+++ musics/serializers.py
@@ -4,20 +4,35 @@ from .models import Music, Artist, Comment
 class MusicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Music
-        fields = ['id', 'title', 'artist']
-        
+        fields = ['id', 'title', 'artist',]
+
 class ArtistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artist
-        fields = ['id', 'name']
-        
+        fields = ['id', 'name',]
+
 class ArtistDetailSerializer(serializers.ModelSerializer):
     music_set = MusicSerializer(many=True)
     class Meta:
         model = Artist
-        fields = ['id', 'name', 'music_set',]
+        fields = ['id', 'name', 'music_set',]    
         
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['id', 'content',]
+
+class MusicDetailSerializer(serializers.ModelSerializer):
+    comment_set = CommentSerializer(many=True)
+    class Meta:
+        model = Music
+        fields = ['id', 'title', 'artist', 'comment_set',]
+
+
+
+
+
+
+
+
+        
